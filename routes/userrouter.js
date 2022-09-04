296a9da99f280593ae7899e6ee8c730e8b6b12ee
Omri Ruvio <omriruvio@gmail.com>
2022-09-04 03:03:45 +0300

    fix: twilio validation

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -23,9 +23,9 @@ router.post('/cropdata', handleIfImage, handleCropData);
 router.get('/history/:phone/:days', handleHistoryRequest);
 // incoming request from twilio flow authentication process
 
-router.post('/identify', twilio.webhook({ validate: shouldValidate }), handleTwilioAuth);
-router.post('/delete-last', twilio.webhook({ validate: shouldValidate }), handleDeleteLast);
-router.post('/mobilesignup', twilio.webhook({ validate: shouldValidate }), handleMobileSignup);
-router.post('/help', twilio.webhook({ validate: shouldValidate }), handleHelpRequest);
+router.post('/identify', twilio.webhook({ validate: shouldValidate, protocol: 'https' }), handleTwilioAuth);
+router.post('/delete-last', twilio.webhook({ validate: shouldValidate, protocol: 'https' }), handleDeleteLast);
+router.post('/mobilesignup', twilio.webhook({ validate: shouldValidate, protocol: 'https' }), handleMobileSignup);
+router.post('/help', twilio.webhook({ validate: shouldValidate, protocol: 'https' }), handleHelpRequest);
 
 module.exports = router;
